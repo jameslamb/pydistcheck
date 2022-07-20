8726849307dfe884d16593bfb7fcfa7cd41f439a
James Lamb <jaylamb20@gmail.com>
2022-07-20 15:40:00 -0500

    set up CLI (#18)

---
+++ src/py_artifact_linter/cli.py
@@ -0,0 +1,20 @@
+import click
+
+
+@click.group()
+def cli():
+    """Command group just used to group all others as sub-commands of ``py-artifact-linter``"""
+    pass
+
+
+@cli.command()
+@click.option(
+    "--file", "-f", default=None, help="Comma-delimited list of doppel output files."
+)
+def check(file: str) -> None:
+    """
+    Check the contents of a distribution.
+    :param file: A file path.
+    """
+    print("running py-artifact-linter")
+    print(file)
