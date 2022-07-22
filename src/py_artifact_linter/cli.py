4ecb9744fab2a660c9c31b33390622c69cb92110
James Lamb <jaylamb20@gmail.com>
2022-07-21 21:32:30 -0500

    get options from pyproject.toml (fixes #24) (#25)
    * get options from pyproject.toml
    
    * fix argument passing
    
    * revert pyproject file

--- src/py_artifact_linter/cli.py
+++ src/py_artifact_linter/cli.py
@@ -1,26 +1,37 @@
+import os
 import click
+from py_artifact_linter._compat import tomllib
 from py_artifact_linter.distribution_summary import summarize_distribution_contents
 
-from typing import Optional
+from typing import Any, Dict, Optional, Union
 
 
 @click.group()
-def cli():
+@click.pass_context
+def cli(ctx):
     """Command group just used to group all others as sub-commands of ``py-artifact-linter``"""
-    pass
+    options: Dict[str, Union[int, str]] = {}
+    if os.path.exists("pyproject.toml"):
+        with open("pyproject.toml", "rb") as f:
+            config_dict = tomllib.load(f)
+            options = config_dict.get("tool", {}).get("py-artifact-linter", {})
+    ctx.obj = options
 
 
 @cli.command()
 @click.option(
     "--file", "-f", default=None, help="Source distribution (.tar.gz) to check"
 )
-def check(file: str) -> None:
+@click.pass_obj
+def check(tool_options: Dict[str, Any], file: str) -> None:
     """
     Check the contents of a distribution.
     :param file: A file path.
     """
     print("running py-artifact-linter")
     print(file)
+    print("pyproject options")
+    print(tool_options)
 
 
 @cli.command()
