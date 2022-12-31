2a0127b383b70cb16ec3af84a7b1363b0381d16b
James Lamb <jaylamb20@gmail.com>
2022-12-31 02:01:03 -0600

    fix path to library

--- tests/data/baseballmetrics/baseballmetrics/_shared_lib.py
+++ tests/data/baseballmetrics/baseballmetrics/_shared_lib.py
@@ -1,3 +1,4 @@
 import ctypes
+from pathlib import Path
 
-_LIB = ctypes.cdll.LoadLibrary("lib_baseballmetrics.so")
+_LIB = ctypes.cdll.LoadLibrary(Path(__file__).absolute().parent.joinpath("lib/lib_baseballmetrics.so"))
