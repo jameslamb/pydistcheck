bd87b92f41df08eec5645d6fd27feaff994c2ac4
James Lamb <jaylamb20@gmail.com>
2023-01-02 00:01:54 -0600

    fix linting

--- tests/data/baseballmetrics/baseballmetrics/_shared_lib.py
+++ tests/data/baseballmetrics/baseballmetrics/_shared_lib.py
@@ -2,5 +2,5 @@ import ctypes
 from pathlib import Path
 
 _LIB = ctypes.cdll.LoadLibrary(
-    Path(__file__).absolute().parent.joinpath("lib/lib_baseballmetrics.so")
+    str(Path(__file__).absolute().parent.joinpath("lib/lib_baseballmetrics.so"))
 )
