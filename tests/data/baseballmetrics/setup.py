4df23118a0f1ae20f05b3e164ba8d7613836bb57
James Lamb <jaylamb20@gmail.com>
2023-01-08 20:01:28 -0600

    [ci] use scikit-build-core for wheel-building (#145)

--- tests/data/baseballmetrics/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_namespace_packages
-from skbuild import setup
-
-setup(
-    cmake_languages=("C", "CXX"),
-    cmake_install_dir="baseballmetrics",
-    include_package_data=False,
-    packages=find_namespace_packages(),
-)
