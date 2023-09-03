6ae7476ba595ca8a727f56b42481dc18501a1da2
James Lamb <jaylamb20@gmail.com>
2023-09-03 00:17:43 -0500

    revert

--- bin/run-smoke-tests.sh
+++ bin/run-smoke-tests.sh
@@ -17,20 +17,31 @@ get-files() {
 # wheel-only packages
 get-files catboost
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols,mixed-file-extensions,too-many-files,unexpected-files' \
+    --max-allowed-size-compressed '100M' \
+    --max-allowed-size-uncompressed '0.5G' \
     ./smoke-tests/*
 
 get-files psycopg2-binary
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols' \
     ./smoke-tests/*
 
 # package where source distro is a .zip
 get-files numpy
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols,unexpected-files' \
+    --max-allowed-files 3000 \
+    --max-allowed-size-uncompressed '150M' \
     ./smoke-tests/*
 
 # package with so many files that `find -exec du -ch` has to batch results
 get-files tensorflow
 pydistcheck \
+    --ignore 'mixed-file-extensions' \
+    --max-allowed-files 10000 \
+    --max-allowed-size-compressed '300M' \
+    --max-allowed-size-uncompressed '1G' \
     ./smoke-tests/*
 
 # packages with lots of bundled non-Python code
@@ -39,16 +50,22 @@ pydistcheck ./smoke-tests/*
 
 get-files Flask
 pydistcheck \
+    --ignore 'unexpected-files' \
     ./smoke-tests/*
 
 # package that isn't actually Python code
 get-files cmake
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols,path-contains-spaces' \
+    --max-allowed-files 4000 \
+    --max-allowed-size-uncompressed '150M' \
     ./smoke-tests/*
 
 # Python clients for other systems
 get-files botocore
 pydistcheck \
+    --max-allowed-files 4000 \
+    --max-allowed-size-uncompressed '150M' \
     ./smoke-tests/*
 
 get-files kafka-python
@@ -60,22 +77,32 @@ pydistcheck ./smoke-tests/*
 # other complex projects that do custom packaging stuff
 get-files apache-airflow
 pydistcheck \
+    --ignore 'mixed-file-extensions' \
     ./smoke-tests/*
 
 get-files astropy
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols,mixed-file-extensions,unexpected-files' \
     ./smoke-tests/*
 
 get-files datatable
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols' \
+    --max-allowed-size-compressed '100M' \
+    --max-allowed-size-uncompressed '100M' \
     ./smoke-tests/*
 
 get-files gensim
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols' \
     ./smoke-tests/*
 
 get-files opencv-python
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols,mixed-file-extensions,unexpected-files' \
+    --max-allowed-files 7500 \
+    --max-allowed-size-compressed '90M' \
+    --max-allowed-size-uncompressed '200M' \
     ./smoke-tests/*
 
 get-files pandas
@@ -83,24 +110,27 @@ pydistcheck ./smoke-tests/*
 
 get-files Pillow
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols' \
     ./smoke-tests/*
 
 get-files pytest
 pydistcheck \
+    --ignore 'mixed-file-extensions,unexpected-files' \
     ./smoke-tests/*
 
 get-files scikit-learn
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols,mixed-file-extensions,unexpected-files' \
     ./smoke-tests/*
 
 get-files Shapely
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols' \
     ./smoke-tests/*
 
 get-files spacy
 pydistcheck \
+    --ignore 'compiled-objects-have-debug-symbols' \
     ./smoke-tests/*
 
 echo "done running smoke tests"
-
-exit 1
