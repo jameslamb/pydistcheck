44fccbbfed498393bac881e6bcc1597c6cce7a54
James Lamb <jaylamb20@gmail.com>
2024-01-27 22:59:50 -0600

    move compiled-object detection code out of distribution_summary

--- src/pydistcheck/shared_lib_utils.py
+++ src/pydistcheck/shared_lib_utils.py
@@ -9,7 +9,7 @@ import zipfile
 from tempfile import TemporaryDirectory
 from typing import List, Tuple
 
-from .distribution_summary import _FileInfo
+from .file_utils import _FileInfo
 
 _COMMAND_FAILED = "__command_failed__"
 _NO_DEBUG_SYMBOLS = "__no_debug_symbols_found__"
