45d5ec7630184f3bb3516e3b9b9ebeaf39651e20
James Lamb <jaylamb20@gmail.com>
2024-07-21 01:36:24 -0500

    more updates

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from datetime import datetime
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
